719804dd2c3aef033c0bc434c5e3eae33f3d9344
trandinhphong0310 <phingtran93@gmail.com>
2025-10-20 16:20:48 +0700

    design homepage

--- Learn-React/src/components/Header/Header.tsx
+++ Learn-React/src/components/Header/Header.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Header() {
 
-    const [showMenu, setShowMenu] = useState<boolean>(false)
-
     return (
-        <header className="bg-black text-white shadow-md text-[16px]">
+        <header className="text-[#2a222b] text-[16px] font-medium">
             <div className='max-w-6xl mx-auto flex justify-between items-center px-6 py-4'>
                 <div className='flex gap-5'>
                     <span className='hover:text-gray-500 cursor-pointer'>
@@ -27,20 +24,14 @@ export default function Header() {
                         </li>
                     </ul>
                 </div>
-                <div
-                    className='relative'
-                    onMouseEnter={() => setShowMenu(true)}
-                    onMouseLeave={() => setShowMenu(false)}>
-                    <button className='hover:text-gray-300 transition cursor-pointer'>Settings</button>
-                    {showMenu && (
-                        <ul className='absolute right-0 w-32 bg-gray-800 rounded-lg shadow-lg overflow-hidden text-sm'>
-                            <li className='px-4 py-2 hover:bg-gray-700 cursor-pointer'>Login</li>
-                            <li className='px-4 py-2 hover:bg-gray-700 cursor-pointer'>Logout</li>
-                            <li className='px-4 py-2 hover:bg-gray-700 cursor-pointer'>Profile</li>
-                        </ul>
-                    )}
+                <div>
+                    <ul className='flex'>
+                        <li className='px-4 py-2 hover:text-purple-300 cursor-pointer'>Log in</li>
+                        <li className='px-4 py-2 bg-[#2a222b] rounded-2xl text-white hover:text-gray-400  hover:scale-90 ease-in-out transition cursor-pointer'>Sign up</li>
+                        <li className='px-4 py-2 hover:text-purple-300 cursor-pointer'>Profile</li>
+                    </ul>
                 </div>
             </div>
-        </header>
+        </header >
     )
 }
--- Learn-React/src/components/Home/Home.tsx
+++ Learn-React/src/components/Home/Home.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import videoHomePage from '../../assets/video-20251020-130204.mp4'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='relative flex items-center justify-between min-h-screen px-20 bg-white overflow-hidden'>
+      <div className='relative w-1/2 flex justify-center'>
+        <video autoPlay muted loop width="651" height="651" className='object-cover '>
+          <source
+            src={videoHomePage}
+            type='video/mp4'
+          />
+        </video>
+      </div>
+      <div className='text-[#2a222b] w-1/2 space-y-6'>
+        <h1 className='text-[72px] font-bold leading-tight'>Get to know your customers with forms worth filling out</h1>
+        <p className='text-[20px]'>Collect all the data you need to understand customers with forms designed to be refreshingly different.</p>
+        <div>
+          <button className='text-[16px] px-6 py-3 text-white bg-[#2a222b] rounded-2xl hover:opacity-70 cursor-pointer hover:scale-90 ease-in-out transition'>Sign up</button>
+          <button className='text-[16px] px-6 py-3 underline hover:text-pink-400 cursor-pointer'>Compare plans</button>
+        </div>
+      </div>
+    </div>
   )
 }
